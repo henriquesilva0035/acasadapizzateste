bf91a05cd3f0f9dab896d9754739e639af3f8fc2
Henrique Nascimento <henriquesilva0035@gmail.com>
2026-01-29 21:09:13 -0300

    teste

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,7 +1,7 @@
 // ARQUIVO: web/src/config.ts
 
 // Seu link fixo e bonitão (Para quem acessa de longe/Vercel https://api-ocachorrao.operaeats.com.br)
-const LINK_ONLINE = 'https://api-teste-ocachorrao.operaeats.com.br';
+const LINK_ONLINE = 'https://api-acasadapizzateste.operaeats.com.br';
 
 // Seu endereço local (Para funcionar SEM INTERNET dentro da loja)
 // IMPORTANTE: O IP do seu computador servidor tem que ser esse mesmo.
